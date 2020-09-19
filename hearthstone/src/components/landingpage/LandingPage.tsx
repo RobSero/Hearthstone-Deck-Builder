4460dd1e0bfbb422f607a3c89736fa0b52710157
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-09-19 14:53:37 +0100

    :fire: NEW - LANDING PAGE LAYOUT STRUCTURED

--- hearthstone/src/components/landingpage/LandingPage.tsx
+++ hearthstone/src/components/landingpage/LandingPage.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import CSS from 'csstype'
 
 const LandingPage: React.FC = () => {
+const [mouseXposition, setX] = React.useState('50%')
+
+const trackMouse = (e:any) => {
+  setX(e.clientX)
+}
+
 return (
-  <div>
-    <h1>Landing Page here</h1>
-    <img src='https://d15f34w2p8l1cc.cloudfront.net/hearthstone/ad8c4355261c52b9c4db3a5c4a4b4a0a6794aa474589c7857b67eea341318a47.png' alt='test'/>
+  <div onMouseMove={trackMouse}>
+    <div id='left-border'></div>
+    <div id='right-border'></div>
+    <div id='top-border'></div>
+    <div id='bottom-border'></div>
+    <button className='start-button'>Start Building</button>
+    <div className='page-container'>
+      <div className='landing-header'>
+      </div>
+      <div className='left-view' style={{width:mouseXposition}}>
+      </div>
+      
+      <div className='right-view'>
+      </div>
+    </div>
   </div>
 )
 }
